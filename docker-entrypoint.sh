38bf84c7c3ca818ad8f8a3690648fe3ff02889df
root <root@mastodon.piensoluegoinstalo.com>
2022-12-12 20:34:37 +0100

    net admin

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+
 if [ -d "/usr/share/easy-rsa/pki" ];
 then
   echo "/usr/share/easy-rsa/pki directory exists."
