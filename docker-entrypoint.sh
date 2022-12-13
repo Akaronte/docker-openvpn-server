92ab5c5677561202574ddcaf1ce24fb4ffc37584
Akaronte <akaronte@gmail.com>
2022-12-13 05:39:35 +0100

    works

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env sh
 
-
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 chmod 600 /dev/net/tun
 
-if [ -d "/usr/share/easy-rsa/pki" ];
+if [ -f "/usr/share/easy-rsa/pki/ca.crt" ];
 then
   echo "/usr/share/easy-rsa/pki directory exists."
 else
   export EASYRSA_BATCH=1
-	echo "/usr/share/easy-rsa/pki directory does not exist."
+  echo "/usr/share/easy-rsa/pki directory does not exist."
   cd /usr/share/easy-rsa/
   sh ./easyrsa init-pki
   sh ./easyrsa gen-dh
   sed 's/^RANDFILE /#&/' -i /usr/share/easy-rsa/openssl-easyrsa.cnf
+  sed 's/$ENV::EASYRSA_PKI/home\/openvpn/g' -i /usr/share/easy-rsa/openssl-easyrsa.cnf
+  cat /usr/share/easy-rsa/openssl-easyrsa.cnf
   sh ./easyrsa build-ca nopass
   sh ./easyrsa gen-req server nopass
   sh ./easyrsa sign-req server server batch
   openvpn --config /etc/openvpn/server.conf
 fi
+
